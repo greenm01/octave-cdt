c0f7c8a1b1f243a4a00b910eb19651d8f21611e5
Mason Austin Green <mason@greenm01.net>
2026-06-10 18:16:32 -0400

    Highlight hole contours in README plot

--- examples/render_readme_hero.m
+++ examples/render_readme_hero.m
@@ -12,23 +12,37 @@ function render_readme_hero (out_path)
   [tri, vertices, boundary_edges] = cdt (polygon, [], [], ...
                                          struct ("keep_boundary_edges", true));
   vertices = rotate_180 (vertices);
+  outer_count = first_ring_count (polygon);
 
   figure (1, "visible", "off");
   clf;
-  cdt_plot (tri, vertices, "color", [0.05 0.22 0.85]);
+  cdt_plot (tri, vertices, "color", [0.58 0.68 0.95]);
   hold on;
 
   for i = 1:rows (boundary_edges)
     edge = boundary_edges(i, :);
-    plot (vertices(edge, 1), vertices(edge, 2), "k-", "linewidth", 1.4);
+    if (all (edge <= outer_count))
+      plot (vertices(edge, 1), vertices(edge, 2), "k-", "linewidth", 1.4);
+    else
+      plot (vertices(edge, 1), vertices(edge, 2), "b-", "linewidth", 2.0);
+    endif
   endfor
 
-  plot (vertices(:, 1), vertices(:, 2), "r.", "markersize", 5);
+  plot (vertices(:, 1), vertices(:, 2), "r.", "markersize", 4);
   axis off;
   title ("Dude With Holes CDT");
   hold off;
 
   print ("-dpng", "-r180", out_path);
+endfunction
+
+function n = first_ring_count (polygon)
+  sep = find (all (isnan (polygon), 2), 1);
+  if (isempty (sep))
+    n = rows (polygon);
+  else
+    n = sep - 1;
+  endif
 endfunction
 
 function rotated = rotate_180 (points)
@@ -38,4 +52,3 @@ function rotated = rotate_180 (points)
   rotated(:, 1) = 2 * center(1) - points(:, 1);
   rotated(:, 2) = 2 * center(2) - points(:, 2);
 endfunction
-
